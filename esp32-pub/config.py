8add74312d3aebe826d88135bff1686c1fda3e58
Gheorghe G <127143941+ghitabn@users.noreply.github.com>
2023-07-26 12:28:56 -0400

    Create config.py

---
+++ esp32-pub/config.py
@@ -0,0 +1,12 @@
+# WiFi configuration
+WIFI_SSID = "xxxxxxxx"    # replace with your own SSID
+WIFI_PASS = "xxxxxxxx"    # replace with your own WiFi password
+
+# MQTT configuration
+MQTT_PORT = "8883"
+MQTT_HOST = "raspberrypi.local"
+MQTT_QOS = 0
+THING_ID = "MyClientDeviceESP-01"
+
+THING_CLIENT_CERT = "MyClientDeviceESP-01-certificate.pem.crt"
+THING_PRIVATE_KEY = "MyClientDeviceESP-01-private.pem.key"
